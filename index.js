04300fe1d1e7cf7654fce394480e7b9828174900
SoundBrake <mesuper222@gmail.com>
2018-07-11 20:21:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+  return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue){
+  return distanceFromHqInBlocks(someValue)*264
+}
+
+function distanceTravelledInFeet(start, end){
+  return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice(start, end){
+  var distance = distanceTravelledInFeet(start, end)
+  if(distance < 400){
+    return 0
+  }
+  else if (distance < 2000){
+    return (distance - 400)*0.02
+  }
+  else if (distance <2500){
+    return 25
+  }
+  else{
+    return "cannot travel that far"
+  }
+}
